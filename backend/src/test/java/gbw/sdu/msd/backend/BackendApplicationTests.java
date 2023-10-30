64a4b9ea4629ec2cce27ba7737637ce7416725bb
lilybw <lilybwanscher@gmail.com>
2023-10-30 18:22:38 +0100

    init
    contains empty projects for the frontend and backend

---
+++ backend/src/test/java/gbw/sdu/msd/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package gbw.sdu.msd.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

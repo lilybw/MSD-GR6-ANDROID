c7701852010320096416b46575266672b932e6ba
thoni21 <90039692+thoni21@users.noreply.github.com>
2023-11-10 15:58:09 +0100

    added notifications view, changed button colors

---
+++ frontend/app/src/main/java/sdu/msd/ui/notifications/NotificationsView.java
@@ -0,0 +1,30 @@
+package sdu.msd.ui.notifications;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import sdu.msd.R;
+import sdu.msd.ui.home.HomeView;
+import sdu.msd.ui.profile.profile;
+
+public class NotificationsView extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_notifications);
+
+        Button btnBack = findViewById(R.id.back);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(NotificationsView.this, HomeView.class);
+                startActivity(intent);
+            }
+        });
+    }
+}

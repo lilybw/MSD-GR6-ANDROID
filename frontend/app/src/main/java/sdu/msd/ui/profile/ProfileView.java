53ab294bf19fe3489e8f8c2b9d9bd2d5a3c76253
Muhammed Alsabban <muals21@student.sdu.dk>
2023-11-17 13:33:56 +0100

    Added The group View

--- frontend/app/src/main/java/sdu/msd/ui/profile/ProfileView.java
+++ frontend/app/src/main/java/sdu/msd/ui/profile/ProfileView.java
@@ -1,19 +1,13 @@
 package sdu.msd.ui.profile;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import sdu.msd.R;
-import sdu.msd.ui.home.HomeView;
 
-public class profile extends AppCompatActivity {
+public class ProfileView extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

247f8d0f07c90bffe0b1cbd8fe3c1ea07e246fb0
thoni21 <90039692+thoni21@users.noreply.github.com>
2023-11-17 15:35:45 +0100

    created group info fragment

--- frontend/app/src/main/java/sdu/msd/ui/home/HomeView.java
+++ frontend/app/src/main/java/sdu/msd/ui/home/HomeView.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
@@ -25,7 +26,9 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import sdu.msd.R;
 import sdu.msd.apiCalls.GroupAPIService;
+import sdu.msd.apiCalls.UserAPIService;
 import sdu.msd.dtos.GroupDTO;
+import sdu.msd.dtos.UserDTO;
 import sdu.msd.ui.Group.GroupView;
 import sdu.msd.ui.createGroup.CreateGroupView;
 import sdu.msd.ui.notifications.NotificationsView;
@@ -36,6 +39,7 @@ public class HomeView extends AppCompatActivity {
     WifiManager wm;
     private static final String API = "http://192.168.185.1:8080/api/v1/";
     private GroupAPIService apiService;
+    private UserAPIService userAPIService;
 
     private static final String BASEURL =  API + "users/";
     int userId;
@@ -48,6 +52,10 @@ public class HomeView extends AppCompatActivity {
         ImageView btnProfile = findViewById(R.id.btnProfile);
         ImageView btnNotifications = findViewById(R.id.btnNotifications);
         Button btnCreateGroup = findViewById(R.id.btnCreateGroup);
+        TextView userNameText = findViewById(R.id.userNameText);
+
+        userNameText.setText("asd");
+
         btnProfile.setOnClickListener(view -> {
             Intent intent = new Intent(HomeView.this, ProfileView.class);
             startActivity(intent);
@@ -73,9 +81,6 @@ public class HomeView extends AppCompatActivity {
         getGroupsOfUser(userId);
 
     }
-
-
-
 
     private void getGroupsOfUser(int userId){
         Call<List<GroupDTO>> call = apiService.getGroupsOfUser(userId);

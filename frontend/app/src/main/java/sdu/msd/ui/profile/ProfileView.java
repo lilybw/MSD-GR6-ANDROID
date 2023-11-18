1d33ac2cf3f023825a95159d7e6ab4e7a80f5b59
Klaes Sørensen <Klaesdrescher@gmail.com>
2023-11-18 13:15:55 +0100

    fixed username scaling

--- frontend/app/src/main/java/sdu/msd/ui/profile/ProfileView.java
+++ frontend/app/src/main/java/sdu/msd/ui/profile/ProfileView.java
@@ -1,11 +1,17 @@
 package sdu.msd.ui.profile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import sdu.msd.R;
+import sdu.msd.ui.home.HomeView;
+import sdu.msd.ui.login.LoginView;
 
 public class ProfileView extends AppCompatActivity {
     @Override
@@ -14,16 +20,38 @@ public class ProfileView extends AppCompatActivity {
         setContentView(R.layout.fragment_profile);
 
         scaleUsernameText();
+
+        Button closeButton = findViewById(R.id.buttonClose); // Go to home
+        closeButton.setOnClickListener(view -> {
+            Intent intent = new Intent(ProfileView.this, HomeView.class);
+            startActivity(intent);
+        });
+
+        Button logoutButton = findViewById(R.id.buttonLogout); // Go to login
+        logoutButton.setOnClickListener(view -> {
+            Intent intent = new Intent(ProfileView.this, LoginView.class);
+            startActivity(intent);
+        });
+
+        Button invoicesButton = findViewById(R.id.buttonInvoices); // Go to invoices
+        invoicesButton.setOnClickListener(view -> {
+            Intent intent = new Intent(ProfileView.this, HomeView.class); // TODO: change to InvoiceView.class when possible.
+            startActivity(intent);
+        });
     }
 
     private void scaleUsernameText() {
-        // Choose the appropriate dimension based on the length of the username
         TextView usernameTextView = findViewById(R.id.username);
-        String username = usernameTextView.getText().toString();
-        float textSize;
-        textSize = (float)((2.0 / username.length()) * (username.length() / 20.0));
-        usernameTextView.setTextSize(textSize);
+        String username = ""; // Replace with the actual username value
+        usernameTextView.setText(username);
 
+        // float textSize = (float)(75.0 / (1.0 + (float)Math.exp(0.4055 * ((float)(username.length()) - 1.0))));
+        float textSize = (float)(100.0 / (1.0 + (float)Math.exp(-(-0.11564339880716944) * ((float)(username.length() - 10.5)))));
+
+        usernameTextView.setTextSize(textSize);
+    }
+
+    private void fetchData() {
 
     }
 }

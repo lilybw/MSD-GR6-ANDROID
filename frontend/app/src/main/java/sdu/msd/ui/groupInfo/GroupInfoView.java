658c92ba1554a0b6adeb9000db27edba8c242dd1
Muhammed Alsabban <muals21@student.sdu.dk>
2023-11-23 20:48:17 +0100

    fixed the button with image

--- frontend/app/src/main/java/sdu/msd/ui/groupInfo/GroupInfoView.java
+++ frontend/app/src/main/java/sdu/msd/ui/groupInfo/GroupInfoView.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
